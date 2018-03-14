00beff7a0af7e5e7dcbd8c471d40531387c8da50
Tyler Griffith <tgriffit@highpoint.edu>
2018-03-14 12:37:57 -0400

    Added Stack and operation methods

--- Calculator.playground/Contents.swift
+++ Calculator.playground/Contents.swift
@@ -2,4 +2,55 @@
 
 import Cocoa
 
-var str = "Hello, playground"
+struct Stack<T> {
+    var items = [T]()
+    
+    mutating func push(newItem: T) {
+        items.append(newItem)
+    }
+    
+    mutating func pop() -> T? {
+        guard !items.isEmpty else {
+            return nil
+        }
+        return items.removeLast()
+    }
+    
+    func top() -> T? {
+        guard !items.isEmpty else{
+            return nil
+        }
+        return items.last
+    }
+    
+    func isEmpty() -> Bool? {
+        guard !items.isEmpty else {
+            return true
+        }
+        return false
+    }
+}
+
+func add(_ a: Int, _ b: Int)->Int{
+    return a+b
+}
+
+func sub(_ a: Int, _ b: Int)->Int{
+    return a-b
+}
+
+func mult(_ a: Int, _ b: Int)->Int{
+    return a*b
+}
+
+func div(_ a: Int, _ b: Int)->Int{
+    
+    if(b != 0){
+        return a/b
+    }else{
+        return 0 //try catch
+    }
+}
+
+
+
