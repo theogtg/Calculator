e83227ab3a698afcf05e07a5990a8666975a181d
Yonatan Belayhun <ybelayhun@magneto.highpoint.edu>
2018-03-27 21:07:36 -0400

    tried the eval button function

--- Calculator/ViewController.swift
+++ Calculator/ViewController.swift
@@ -32,11 +32,132 @@ struct Stack<T> {
         return false
     }
 }
+func add(_ a: Double, _ b: Double)->Double{
+    return a+b
+}
 
+func sub(_ a: Double, _ b: Double)->Double{
+    return a-b
+}
+
+func mult(_ a: Double, _ b: Double)->Double{
+    return a*b
+}
+
+func div(_ a: Double, _ b: Double)->Double{
+    
+    if(b != 0){
+        return a/b
+    }else{
+        return 0 //try catch
+    }
+}
+
+typealias binop = (Double, Double)->Double
+let ops: [String: binop] = ["+":add, "-":sub, "*":mult, "/":div]
+var opFunc = ops["+"]
+func doMath(_ a: Double, _ b: Double, _ op: String) -> Double
+{
+    let opFunc = ops["\(op)"]
+    return opFunc!(a,b)
+}
+
+func postFixEval(_ tokens: [String]) -> Double
+{
+    var stack = Stack<Double>()
+    for token in tokens{
+        if let num = Double(token) {
+            stack.push(newItem: num)
+        } else {
+            let val2 = stack.pop()!
+            let val1 = stack.pop()!
+            //let b = doMath(val1, val2, token)
+            stack.push(newItem: doMath(val1, val2, token))
+            //stack.push(newItem: b)
+        }
+    }
+    return stack.pop()!
+}
+
+func priority(_ char: String)->Int{
+    switch char {
+    case "(":
+        return 20
+    case "+":
+        return 12
+    case "-":
+        return 12
+    case "*":
+        return 13
+    case "/":
+        return 13
+    case ")":
+        return 19
+    default:
+        return 0
+    }
+}
+
+func inFixEval(_ tokens: [String]) -> Double
+{
+    var operand = Stack<Double>()
+    var operater = Stack<String>()
+    
+    //while there is another element in the infixExpr
+    for token in tokens
+    {
+        //if the token is a double
+        if let num = Double(token)
+        {
+            //push onto stack
+            operand.push(newItem: num)
+        }else if token == ")"
+        {
+            while operater.top() != "("
+            {
+                //pop 2 values off the stack
+                let val2 = operand.pop()!
+                let val1 = operand.pop()!
+                //pop an operator
+                let operater1 = operater.pop()!
+                //push result
+                operand.push(newItem: doMath(val1, val2, operater1))
+            }
+            //pop the "(" off the stack
+            operater.pop()!
+        }else{
+            while(operater.isEmpty() != true &&
+                priority(operater.top()!) >= priority(token) &&
+                operater.top() != "(")
+            {
+                //pop 2 values off the stack
+                let val2 = operand.pop()!
+                let val1 = operand.pop()!
+                //pop an operator
+                let operater1 = operater.pop()!
+                //push result
+                operand.push(newItem: doMath(val1, val2, operater1))
+            }
+            //push the current token on the operator stack
+            operater.push(newItem: token)
+        }
+        //Print the stacks
+        //print("Operators:" , operater.items)
+        //print("Operands:" , operand.items)
+    }
+    //Taking care of any operations pending
+    while(operater.isEmpty() != true) {
+        let val2 = operand.pop()!
+        let val1 = operand.pop()!
+        let operater1 = operater.pop()!
+        operand.push(newItem: doMath(val1, val2, operater1))
+    }
+    return operand.pop()!
+}
 class ViewController: UIViewController {
 
     @IBOutlet weak var label: UITextField!
-    var numOnScreen: Double = 0
+    
     @IBAction func numbers(_ sender: UIButton) {
         label.text = label.text! + String(sender.tag)
     }
@@ -69,6 +190,10 @@ class ViewController: UIViewController {
         if sender.tag == 17{
             label.text = label.text! + "."
         }
+    }
+    @IBAction func Eval(_ sender: UIButton) {
+        let numOnScreen = [label.text!]
+        label.text = String(inFixEval(numOnScreen))
     }
     override func viewDidLoad() {
         super.viewDidLoad()
